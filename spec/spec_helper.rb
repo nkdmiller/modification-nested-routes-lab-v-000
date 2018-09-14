4751baca92183e328057e1e47b73457323487066
Nate Miller <natekmiller96@gmail.com>
2018-09-14 02:56:30 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -90,5 +90,7 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
+  config.color_enabled? = true
 =end
+  
 end
