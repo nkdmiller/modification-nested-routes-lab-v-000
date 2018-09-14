925a3ce427de131a56a3f0d32348c493fa252ef5
Nate Miller <natekmiller96@gmail.com>
2018-09-14 02:53:43 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,6 @@
 module SongsHelper
  def artist_select(song, artist)
-   if song.artist.nil?
+   if artist.nil?
      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, "id", "name")
    else
      hidden_field_tag "song[artist_id]", song.artist_id
